d144ba601b4362120b0523497a4aa276b11f37d0
mura-work <murata.naoya.work@gmail.com>
2023-02-25 16:50:25 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<script setup>
+const version = 2 + 1;
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 10rem;
+}
+</style>
